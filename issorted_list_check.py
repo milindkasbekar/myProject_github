a1be96a215cc4ef4b6f4627f47ab74ef0582da18
Milind <40655893+milindkasbekar@users.noreply.github.com>
2022-01-18 12:22:20 +0530

    Add files via upload

---
+++ issorted_list_check.py
@@ -0,0 +1,13 @@
+# determine if a list is sorted
+
+
+items1 = [6, 8, 19, 20, 23, 41, 49, 53, 56, 87]
+items2 = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
+
+def is_sorted(itemlist):
+    # using the all function
+    return all(itemlist[i] <= itemlist[i+1] for i in range(len(itemlist)-1))
+
+print(is_sorted(items1))
+print(is_sorted(items2))
+
